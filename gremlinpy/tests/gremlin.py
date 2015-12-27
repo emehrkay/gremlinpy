7f665dca2ffd644f269cd52552e900354af21932
Mark Henderson <mark@oneilinteractive.com>
2015-12-27 08:10:40 -0800

    assertTrue to assertEqual

--- gremlinpy/tests/gremlin.py
+++ gremlinpy/tests/gremlin.py
@@ -13,41 +13,42 @@ def get_dict_key(dict, value):
 
 
 class GremlinTests(unittest.TestCase):
+
     def test_gremlin_instance(self):
         g = Gremlin()
 
-        self.assertTrue(type(g) == Gremlin)
+        self.assertEqual(type(g), Gremlin)
 
     def test_can_drop_graph_variable(self):
         g = Gremlin().set_graph_variable('')
 
-        self.assertTrue(str(g) == '')
+        self.assertEqual(str(g), '')
 
     def test_can_change_graph_variable(self):
         g = Gremlin().set_graph_variable('x')
 
-        self.assertTrue(str(g) == 'x')
+        self.assertEqual(str(g), 'x')
 
     def test_can_add_one_attribute(self):
-        g        = Gremlin().a
+        g = Gremlin().a
         expected = 'g.a'
 
-        self.assertTrue(str(g) == expected)
+        self.assertEqual(str(g), expected)
 
     def test_can_add_one_attribute_and_drop_graph_variable(self):
-        g        = Gremlin().a.set_graph_variable('')
+        g = Gremlin().a.set_graph_variable('')
         expected = 'a'
 
-        self.assertTrue(str(g) == expected)
+        self.assertEqual(str(g), expected)
 
     def test_can_add_two_attributes(self):
-        g        = Gremlin().a.b
+        g = Gremlin().a.b
         expected = 'g.a.b'
 
-        self.assertTrue(str(g) == expected)
+        self.assertEqual(str(g), expected)
 
     def test_can_add_random_attributes(self):
-        g   = Gremlin()
+        g = Gremlin()
         exp = ['g']
 
         for x in range(1, randrange(5, 22)):
@@ -56,10 +57,10 @@ class GremlinTests(unittest.TestCase):
 
         expected = '.'.join(exp)
 
-        self.assertTrue(str(g) == expected)
+        self.assertEqual(str(g), expected)
 
     def test_can_add_random_attributes_drop_graph_variable(self):
-        g   = Gremlin().set_graph_variable('')
+        g = Gremlin().set_graph_variable('')
         exp = []
 
         for x in range(1, randrange(5, 22)):
@@ -68,7 +69,7 @@ class GremlinTests(unittest.TestCase):
 
         expected = '.'.join(exp)
 
-        self.assertTrue(str(g) == expected)
+        self.assertEqual(str(g), expected)
 
     def test_can_add_return_variable(self):
         g = Gremlin()
@@ -114,25 +115,25 @@ class GremlinTests(unittest.TestCase):
         self.assertEqual(len(g.bound_params), 0)
 
     def test_can_add_functon_one_arg(self):
-        g           = Gremlin().function('arg')
-        string      = str(g)
+        g = Gremlin().function('arg')
+        string = str(g)
         bind, value = g.bound_params.copy().popitem()
-        expected    = 'g.function(%s)' % bind
+        expected = 'g.function(%s)' % bind
 
-        self.assertTrue(string == expected)
-        self.assertTrue(len(g.bound_params) == 1)
+        self.assertEqual(string, expected)
+        self.assertEqual(len(g.bound_params), 1)
 
     def test_can_add_unbound_functon_one_arg(self):
-        g        = Gremlin().unbound('function', 'arg')
-        string   = str(g)
+        g = Gremlin().unbound('function', 'arg')
+        string = str(g)
         expected = 'g.function(arg)'
 
         self.assertEqual(string, expected)
         self.assertEqual(len(g.bound_params), 0)
 
     def test_can_add_raw_functon_one_arg(self):
-        g        = Gremlin().func_raw('function', 'arg')
-        string   = str(g)
+        g = Gremlin().func_raw('function', 'arg')
+        string = str(g)
 
         bind, value = g.bound_params.copy().popitem()
         expected = 'gfunction(%s)' % bind
@@ -141,25 +142,25 @@ class GremlinTests(unittest.TestCase):
         self.assertEqual(len(g.bound_params), 1)
 
     def test_can_add_functon_two_args(self):
-        g           = Gremlin().function('one', 'two')
-        string      = str(g)
+        g = Gremlin().function('one', 'two')
+        string = str(g)
         bind, value = g.bound_params.copy().popitem()
-        expected    = 'g.function(one, %s)' % bind
+        expected = 'g.function(one, %s)' % bind
 
-        self.assertTrue(string == expected)
-        self.assertTrue(len(g.bound_params) == 1)
+        self.assertEqual(string, expected)
+        self.assertEqual(len(g.bound_params), 1)
 
     def test_can_add_unbound_functon_two_args(self):
-        g        = Gremlin().unbound('function', 'arg', 'two')
-        string   = str(g)
+        g = Gremlin().unbound('function', 'arg', 'two')
+        string = str(g)
         expected = 'g.function(arg, two)'
 
         self.assertEqual(string, expected)
         self.assertEqual(len(g.bound_params), 0)
 
     def test_can_add_raw_functon_two_args(self):
-        g        = Gremlin().func_raw('function', 'arg', 'two')
-        string   = str(g)
+        g = Gremlin().func_raw('function', 'arg', 'two')
+        string = str(g)
 
         bind, value = g.bound_params.copy().popitem()
         expected = 'gfunction(arg, %s)' % bind
@@ -168,13 +169,13 @@ class GremlinTests(unittest.TestCase):
         self.assertEqual(len(g.bound_params), 1)
 
     def test_can_add_functon_three_args(self):
-        g           = Gremlin().function('one', 'two', 'three')
-        string      = str(g)
+        g = Gremlin().function('one', 'two', 'three')
+        string = str(g)
         bind, value = g.bound_params.copy().popitem()
-        expected    = 'g.function(one, two, %s)' % bind
+        expected = 'g.function(one, two, %s)' % bind
 
-        self.assertTrue(string == expected)
-        self.assertTrue(len(g.bound_params) == 1)
+        self.assertEqual(string, expected)
+        self.assertEqual(len(g.bound_params), 1)
 
     def test_can_add_functon_manually_bind_one_arg(self):
         g = Gremlin()
@@ -192,53 +193,53 @@ class GremlinTests(unittest.TestCase):
         self.assertEqual(len(g.bound_params), 1)
 
     def test_can_add_functon_manually_bind_two_args(self):
-        g      = Gremlin()
-        bound  = g.bind_param('arg')
+        g = Gremlin()
+        bound = g.bind_param('arg')
         bound2 = g.bind_param('arg2')
 
         g.function(bound[0], bound2[0])
 
-        string   = str(g)
+        string = str(g)
         expected = 'g.function(%s, %s)' % (bound[0], bound2[0])
 
-        self.assertTrue(string == expected)
-        self.assertTrue(len(g.bound_params) == 2)
+        self.assertEqual(string, expected)
+        self.assertEqual(len(g.bound_params), 2)
 
     def test_can_add_function_with_arg_before_attribute(self):
         g = Gremlin()
 
         g.function('val').a
 
-        string      = str(g)
+        string = str(g)
         bind, value = g.bound_params.copy().popitem()
-        expected    = 'g.function(%s).a' % bind
+        expected = 'g.function(%s).a' % bind
 
-        self.assertTrue(string == expected)
-        self.assertTrue(len(g.bound_params) == 1)
+        self.assertEqual(string, expected)
+        self.assertEqual(len(g.bound_params), 1)
 
     def test_can_add_function_with_arg_after_attribute(self):
         g = Gremlin()
 
         g.a.function('val')
 
-        string      = str(g)
+        string = str(g)
         bind, value = g.bound_params.copy().popitem()
-        expected    = 'g.a.function(%s)' % bind
+        expected = 'g.a.function(%s)' % bind
 
-        self.assertTrue(string == expected)
-        self.assertTrue(len(g.bound_params) == 1)
+        self.assertEqual(string, expected)
+        self.assertEqual(len(g.bound_params), 1)
 
     def test_can_add_function_with_arg_after_attribute_drop_graph_var(self):
         g = Gremlin().set_graph_variable('')
 
         g.a.function('val')
 
-        string      = str(g)
+        string = str(g)
         bind, value = g.bound_params.copy().popitem()
-        expected    = 'a.function(%s)' % bind
+        expected = 'a.function(%s)' % bind
 
-        self.assertTrue(string == expected)
-        self.assertTrue(len(g.bound_params) == 1)
+        self.assertEqual(string, expected)
+        self.assertEqual(len(g.bound_params), 1)
 
     def test_can_add_function_where_no_params_are_bound(self):
         g = Gremlin()
@@ -247,8 +248,8 @@ class GremlinTests(unittest.TestCase):
 
         expected = 'g.function(val1, val2)'
 
-        self.assertTrue(str(g) == expected)
-        self.assertTrue(len(g.bound_params) == 0)
+        self.assertEqual(str(g), expected)
+        self.assertEqual(len(g.bound_params), 0)
 
     def test_can_add_function_where_no_params_are_bound_and_func_with_bound_params(self):
         g = Gremlin()
@@ -268,13 +269,13 @@ class GremlinTests(unittest.TestCase):
 
         g.condition('x').close('body')
 
-        s        = str(g)
-        params   = g.bound_params
-        x        = get_dict_key(params, 'x')
+        s = str(g)
+        params = g.bound_params
+        x = get_dict_key(params, 'x')
         expected = 'g.condition(%s){body}' % x
 
-        self.assertTrue(s == expected)
-        self.assertTrue(len(params) == 1)
+        self.assertEqual(s, expected)
+        self.assertEqual(len(params), 1)
 
     def test_can_add_an_index_with_single_value(self):
         g = Gremlin()
@@ -284,7 +285,7 @@ class GremlinTests(unittest.TestCase):
         s = str(g)
         expected = 'g.function()[1]'
 
-        self.assertTrue(s == expected)
+        self.assertEqual(s, expected)
 
     def test_can_add_an_index_with_range(self):
         g = Gremlin()
@@ -294,7 +295,7 @@ class GremlinTests(unittest.TestCase):
         s = str(g)
         expected = 'g.function()[1..2]'
 
-        self.assertTrue(s == expected)
+        self.assertEqual(s, expected)
 
     def test_can_add_an_index_after_closure(self):
         g = Gremlin()
@@ -315,12 +316,12 @@ class GremlinTests(unittest.TestCase):
 
         g.function().raw(r)
 
-        s        = str(g)
-        params   = g.bound_params
+        s = str(g)
+        params = g.bound_params
         expected = 'g.function()%s' % r
 
-        self.assertTrue(s == expected)
-        self.assertTrue(len(params) == 0)
+        self.assertEqual(s, expected)
+        self.assertEqual(len(params), 0)
 
     def test_can_add_raw_drop_graph_variable(self):
         g = Gremlin()
@@ -328,12 +329,12 @@ class GremlinTests(unittest.TestCase):
 
         g.raw(r).set_graph_variable('')
 
-        s        = str(g)
-        params   = g.bound_params
+        s = str(g)
+        params = g.bound_params
         expected = '%s' % r
 
-        self.assertTrue(s == expected)
-        self.assertTrue(len(params) == 0)
+        self.assertEqual(s, expected)
+        self.assertEqual(len(params), 0)
 
     def test_can_add_raw_between_functions(self):
         g = Gremlin()
@@ -342,9 +343,9 @@ class GremlinTests(unittest.TestCase):
 
         g.function().raw(r).func2(a)
 
-        s        = str(g)
-        params   = g.bound_params
-        arg      = get_dict_key(params, a)
+        s = str(g)
+        params = g.bound_params
+        arg = get_dict_key(params, a)
         expected = 'g.function()%sfunc2(%s)' % (r, arg)
 
         self.assertEqual(s, expected)
@@ -370,13 +371,13 @@ class GremlinTests(unittest.TestCase):
 
         g.add_token(init)
 
-        s        = str(g)
-        params   = g.bound_params
-        arg      = get_dict_key(params, 'arg')
+        s = str(g)
+        params = g.bound_params
+        arg = get_dict_key(params, 'arg')
         expected = 'g.__init__(%s)' % arg
 
-        self.assertTrue(s == expected)
-        self.assertTrue(len(params) == 1)
+        self.assertEqual(s, expected)
+        self.assertEqual(len(params), 1)
 
     def test_can_add_a_reserved_word_as_apart_of_the_query_as_function_with_args_after_other_calls(self):
         g = Gremlin();
@@ -385,15 +386,15 @@ class GremlinTests(unittest.TestCase):
 
         g.look(args[1]).at(args[2]).close('--this--').add_token(init)
 
-        s        = str(g)
-        params   = g.bound_params
-        arg      = get_dict_key(params, args[0])
-        look     = get_dict_key(params, args[1])
-        at       = get_dict_key(params, args[2])
+        s = str(g)
+        params = g.bound_params
+        arg = get_dict_key(params, args[0])
+        look = get_dict_key(params, args[1])
+        at = get_dict_key(params, args[2])
         expected = 'g.look(%s).at(%s){--this--}.__init__(%s)' % (look, at, arg)
 
-        self.assertTrue(s == expected)
-        self.assertTrue(len(params) == 3)
+        self.assertEqual(s, expected)
+        self.assertEqual(len(params), 3)
 
     def test_can_add_a_reserved_word_as_apart_of_the_query_as_function_without_args(self):
         g = Gremlin();
@@ -402,12 +403,12 @@ class GremlinTests(unittest.TestCase):
 
         g.add_token(init)
 
-        s        = str(g)
-        params   = g.bound_params
+        s = str(g)
+        params = g.bound_params
         expected = 'g.__init__(%s, %s)' % (unbound[0], unbound[1])
 
-        self.assertTrue(s == expected)
-        self.assertTrue(len(params) == 0)
+        self.assertEqual(s, expected)
+        self.assertEqual(len(params), 0)
 
     def test_can_add_a_reserved_word_as_apart_of_the_query_as_function_without_args_after_other_calls(self):
         g = Gremlin();
@@ -417,12 +418,12 @@ class GremlinTests(unittest.TestCase):
 
         g.someFunc(arg).add_token(init)
 
-        s        = str(g)
-        params   = g.bound_params
+        s = str(g)
+        params = g.bound_params
         expected = 'g.someFunc(%s).__init__(%s, %s)' % (get_dict_key(params, arg), unbound[0], unbound[1])
 
-        self.assertTrue(s == expected)
-        self.assertTrue(len(params) == 1)
+        self.assertEqual(s, expected)
+        self.assertEqual(len(params), 1)
 
 
 class GremlinInjectionTests(unittest.TestCase):
@@ -448,11 +449,11 @@ class GremlinInjectionTests(unittest.TestCase):
         g.nest(n.nested(d.deep()))
 
         expected = 'g.nest(g.nested(g.deep()))'
-        string   = str(g)
-        params   = g.bound_params
+        string = str(g)
+        params = g.bound_params
 
-        self.assertTrue(expected == string)
-        self.assertTrue(len(params) == 0)
+        self.assertEqual(expected, string)
+        self.assertEqual(len(params), 0)
 
     def test_can_nest_with_bound_params(self):
         g = Gremlin()
@@ -469,8 +470,8 @@ class GremlinInjectionTests(unittest.TestCase):
         child = get_dict_key(params, 'child')
         expected = 'g.function(name, %s).nest(setSubProp(prop, %s))' % (name, child)
 
-        self.assertTrue(expected == string)
-        self.assertTrue(len(params) == 2)
+        self.assertEqual(expected, string)
+        self.assertEqual(len(params), 2)
 
 
 class PredicateTests(unittest.TestCase):
@@ -514,7 +515,7 @@ class PredicateTests(unittest.TestCase):
         expected = 'g.function(lt(%s).out(lt(%s)))' % (argument, argument2)
 
         self.assertEqual(expected, string)
-        
+
 
 
 if __name__ == '__main__':
