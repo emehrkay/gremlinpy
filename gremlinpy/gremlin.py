0e2a43f682c89de4ae4d5ebac7859e3ca4bf913b
Asaf Rafaeli <asaf@email>
2016-11-01 15:37:26 +0200

    Added aliases for 'from' and 'to' steps

--- gremlinpy/gremlin.py
+++ gremlinpy/gremlin.py
@@ -599,6 +599,20 @@ class IN(Predicate):
     def _function(self, *args):
         return 'in'
 
+class TO(Predicate):
+    """Allows for easy use of 'to' in steps"""
+
+    @property
+    def _function(self, *args):
+        return 'to'
+
+class FROM(Predicate):
+    """Allows for easy use of 'from' in steps"""
+
+    @property
+    def _function(self, *args):
+        return 'from'
+
 
 def _(method, *args):
     """method used to create predicates dynamically"""
